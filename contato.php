c017f275fb46a3fbf22ffd0678af58192f71a8ab
Fabiano R <fabiano.r@gmail.com>
2014-06-27 20:07:56 -0700

    criando o form com arquivos

---
+++ contato.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Entre em contato</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron-narrow.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+		<?php require_once('header.php');?>
+      </div>
+
+
+
+      <div class="row">
+      <h2>Contato<small> Fale conosco</small></h2>     
+      </div>
+      
+      <div class="row">
+      <div class="col-md-8">
+      
+<form role="form">
+       <div class="form-group">
+        <label for="exampleInputEmail1">Nome</label>
+        <input required type="text" class="form-control" name="nome" id="nome" placeholder="Seu nome">
+      	</div>
+      
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email</label>
+        <input required type="email" class="form-control" name="email" id="email" placeholder="Seu e-mail">
+      </div>
+
+       <div class="form-group">
+        <label for="exampleInputEmail1">Assunto</label>
+        <input required type="text" class="form-control" id="assunto" name="assunto" placeholder="Seu assunto">
+      	</div>   
+
+       <div class="form-group">
+        <label for="exampleInputEmail1">Comentário</label>
+        <textarea required class="form-control" name="comentario" id="comentario" placeholder="Sua mensagem aqui..." style="height:100px;"></textarea>
+      	</div>            
+      
+     
+      <button type="submit" class="btn btn-primary btn-lg">Enviar contato</button>
+    </form>
+          
+      </div>
+      <div class="col-md-4">      
+          <address>
+              <strong>Exemple, Inc.</strong><br>
+              795 Folsom Ave, Suite 600<br>
+              San Francisco, CA 94107<br>
+              <abbr title="Phone">P:</abbr> (123) 456-7890
+          </address>
+      </div>
+    </div>
+
+      <div class="footer">
+       <?php require_once('footer.php'); ?>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
---
+++ servicos.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Serviços</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron-narrow.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+		<?php require_once('header.php');?>
+      </div>
+
+
+
+      <div class="row">
+      <h2>Serviços<small> Nossa área de atuação</small></h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque venenatis porta. Proin eget turpis sed est rutrum sagittis. In id erat vitae metus mollis varius. Vivamus iaculis arcu risus, at varius tortor ullamcorper et. Pellentesque quis lacinia lectus. Nam malesuada sagittis velit, a aliquam erat euismod eu. Nullam ornare id neque non posuere. Aenean placerat neque at metus aliquet cursus. Cras turpis purus, imperdiet non consectetur id, feugiat vulputate massa. Vivamus semper condimentum dictum. Sed eleifend et risus in pulvinar. Nam eu risus arcu. Mauris cursus, nibh in commodo laoreet, turpis nibh ornare leo, vitae vulputate ipsum dui scelerisque est. Donec tincidunt eros arcu, tempus rhoncus augue pharetra non. Donec vitae risus non sapien cursus tempus sed eu enim. Integer vel libero nulla.
+
+Curabitur vulputate pulvinar dolor vel rhoncus. Proin non venenatis massa, sit amet consequat nunc. Vivamus placerat dui mi. Donec pulvinar sem vestibulum turpis imperdiet auctor. Integer libero orci, vulputate ac interdum sed, convallis vel ipsum. Aenean sit amet orci ac metus malesuada porta. Vestibulum dictum purus vitae nisl commodo pulvinar. Proin imperdiet non felis mollis semper. Nulla porta tellus sit amet enim congue suscipit. Integer elementum neque vitae sapien varius gravida. Nam vestibulum tempor dolor quis vestibulum. Proin ullamcorper urna ac feugiat aliquam. Proin vitae velit ut mauris feugiat pellentesque a vitae neque. Aliquam venenatis aliquam turpis sit amet aliquam. Donec malesuada sapien id diam vestibulum, in laoreet lorem aliquam. Cras turpis purus, suscipit ac elit sed, tempus imperdiet nunc.
+
+Sed ut auctor metus, sed pulvinar nulla. Curabitur sagittis semper venenatis. Nulla dignissim ac nulla nec accumsan. Proin iaculis, orci at venenatis dictum, mi nisi dapibus massa, fringilla suscipit mauris massa id magna. Donec ac auctor lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer orci urna, vestibulum vitae sem ac, accumsan iaculis turpis. Curabitur at tempor velit, nec tincidunt est. Morbi sit amet diam nec magna fringilla tempus at sed erat. Mauris eget quam rhoncus ligula rutrum euismod ut eu ipsum. Nam justo sapien, sollicitudin in lacinia non, malesuada molestie mauris.</p>
+      </div>
+
+      <div class="footer">
+       <?php require_once('footer.php'); ?>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
