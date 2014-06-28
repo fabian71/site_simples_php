ea7ef5f1605fbce1c023d5014c31401bbfc7fece
Fabiano R <fabiano.r@gmail.com>
2014-06-27 20:07:36 -0700

    criando o form

--- index.php
+++ index.php
@@ -13,6 +13,21 @@ if($_GET['pag'] == 'home'){
 	
 	header('Location: empresa.php');
 	
+}elseif($_GET['pag'] == 'produtos'){
+	
+	header('Location: produtos.php');
+	
+}elseif($_GET['pag'] == 'servicos'){
+	
+	header('Location: servicos.php');
+	
+}
+elseif($_GET['pag'] == 'contato'){
+	
+	header('Location: contato.php');
+	
+}else{
+	header('Location: 404.php');
 }
 
 
