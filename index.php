84fcd9cfad069865a604160a7dfde75783c1f459
Fabiano <fabian71@hotmail.com>
2014-07-16 00:51:20 -0300

    criando paginas de login

--- index.php
+++ index.php
@@ -28,7 +28,8 @@ function rota($url){
 				'servicos' => 'servicos.php',
 				'contato' => 'contato.php',
 				'fixtures' => 'fixtures.php',
-				'busca' => 'busca.php'
+				'busca' => 'busca.php',
+				'admin' => 'admin/index.php'
 				];
 	
 	//carrega o home
@@ -61,48 +62,4 @@ function rota($url){
 			require_once('404.php');
 	}
 
-
-	
-		
-
-
 }
-exit;
-
-
-
-if((isset($_GET))){
-	header('Location: home.php');
-}
-
-if(($_GET['pag'] == 'home') OR ($_GET['pag'] == '')){
-	
-	header('Location: home.php');
-
-}elseif($_GET['pag'] == 'empresa'){
-	
-	header('Location: empresa.php');
-	
-}elseif($_GET['pag'] == 'produtos'){
-	
-	header('Location: produtos.php');
-	
-}elseif($_GET['pag'] == 'servicos'){
-	
-	header('Location: servicos.php');
-	
-}
-elseif($_GET['pag'] == 'contato'){
-	
-	header('Location: contato.php');
-	
-}else{
-	//header("HTTP/1.0 404 Not Found");
-	
-	header('HTTP/1.0 404 Not Found');
-    echo "<h1>OPS! 404 </h1>";
-    echo "Desculpe, mas n&atilde;o foi poss&iacute;vel encontrar a p&aacute;gina que voc&ecirc; solicitou.";
-    exit();	
-	//header('Location: 404.php');
-}
-?>
